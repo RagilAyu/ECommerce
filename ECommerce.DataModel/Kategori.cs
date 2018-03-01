19f52af001a893f6bfd2663c34a4ad65a7fc3b5c
RagilAyu <ragilayu.ra@gmail.com>
2018-03-01 17:43:34 +0700

    coba

--- ECommerce.DataModel/Kategori.cs
+++ ECommerce.DataModel/Kategori.cs
@@ -19,6 +19,8 @@ namespace ECommerce.DataModel
             [Column(TypeName = "varchar"), MaxLength(50)]
             public string Deskripsi { get; set; }
 
+            public int MyProperty { get; set; }
+
             public virtual ICollection<Barang> Barangs { get; set; }
       }
 }
